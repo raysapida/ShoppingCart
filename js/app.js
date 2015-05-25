1296cd8bfe81d285d67d6fb4bc38b75d1b5731da
Raymond Sapida <ray.sapida@gmail.com>
2015-05-24 18:48:09 -0700

    Add ability to delete list item and add click handler for check and times

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
   $( "#item" ).submit(function( event ) {
     var x = $("input").val();
-    var del_btn = "<button type='submit' class='fa fa-trash-o'></button>";
-    var check_btn = "<button type='submit' class='fa fa-check-circle-o'></button>";
+    var del_btn = "<button class='fa fa-trash-o'></button>";
+    var check_btn = "<button class='fa fa-check-circle-o'></button>";
     $('.list').append("<li>"+x+del_btn+check_btn+"</li>");
     event.preventDefault();
+  });
+  $('.list').on('click', '.fa-trash-o', function(){
+    $(this).parent().remove();
+  });
+  $('.list').on('click', '.fa-times', function(){
+    alert('times has been clicked');
+  });
+  $('.list').on('click', '.fa-check-circle-o', function(){
+    alert('check has been clicked');
   });
 });
