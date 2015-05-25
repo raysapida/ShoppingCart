cc1cc56ccc545dcae8d12f1ae5ae1a368c0f16e6
Raymond Sapida <ray.sapida@gmail.com>
2015-05-24 19:28:27 -0700

    Add complete and incomplete function with two different ul lists

--- js/app.js
+++ js/app.js
@@ -1,18 +1,31 @@
 $(document).ready(function() {
+  var del_btn = "<button class='fa fa-trash-o'></button>";
+  var check_btn = "<button class='fa fa-check-circle-o'></button>";
+  var times_btn = "<button class='fa fa-times'></button>";
+
   $( "#item" ).submit(function( event ) {
     var x = $("input").val();
-    var del_btn = "<button class='fa fa-trash-o'></button>";
-    var check_btn = "<button class='fa fa-check-circle-o'></button>";
     $('.list').append("<li>"+x+del_btn+check_btn+"</li>");
     event.preventDefault();
   });
+
   $('.list').on('click', '.fa-trash-o', function(){
     $(this).parent().remove();
   });
-  $('.list').on('click', '.fa-times', function(){
-    alert('times has been clicked');
-  });
+
   $('.list').on('click', '.fa-check-circle-o', function(){
-    alert('check has been clicked');
+    var item = $(this).parent().text();
+    $(this).parent().remove();
+    $('.complete-list').append("<li class='complete'>"+item+del_btn+times_btn+"</li>");
+  });
+
+  $('.complete-list').on('click', '.fa-times', function(){
+    var item = $(this).parent().text();
+    $(this).parent().remove();
+    $('.list').append("<li>"+item+del_btn+check_btn+"</li>");
+  });
+
+  $('.complete-list').on('click', '.fa-trash-o', function(){
+    $(this).parent().remove();
   });
 });
